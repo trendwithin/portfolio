adf0e130cf1768a482f54a8095dfda9349db6bd7
Michael Becco <e4e6d4d5@gmail.com>
2015-03-05 14:53:08 -0800

    Add First Test Case

---
+++ test/features/visiting_the_article_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+#given the following
+feature "Clickning Blog Link on Home Page" do
+  #when this happens
+  scenario "Renders Article page" do
+    #then expect this behavior
+    visit '/'
+    click_link('Blog')
+    page.must_have_content "Hello World"
+    page.wont_have_content "Goobye All!"
+  end
+end
+
+
