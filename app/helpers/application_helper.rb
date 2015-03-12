53890e64b152e254635bc1322a2f84cb8d8a4cdd
Michael Becco <e4e6d4d5@gmail.com>
2015-03-12 15:01:40 -0700

    Refactor Code
    modify .git ingore file
    clean code base

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def flash_case(level)
-  case level
-  when :notice then "info"
-  when :success then "success"
-  when :error then "error"
-  when :alert then "warning"
+    case level
+    when :notice then "info"
+    when :success then "success"
+    when :error then "error"
+    when :alert then "warning"
+    end
   end
-end
 end
